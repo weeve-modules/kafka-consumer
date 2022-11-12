d72bb8132b0ac89aa30be7b01aca30de58dab10e
kubagrzelak <jakubgrzelak1@gmail.com>
2022-11-12 20:07:04 +0000

    Working solution

--- src/module/module.py
+++ src/module/module.py
@@ -7,9 +7,11 @@ Edit this file to implement your module.
 
 from logging import getLogger
 from api.send_data import send_data
+from kafka import KafkaConsumer
+from json import loads
+from os import getenv
 
 log = getLogger("module")
-
 
 def module_main():
     """
@@ -20,22 +22,26 @@ def module_main():
     log.debug("Inputting data...")
 
     try:
-        # YOUR CODE HERE
-        # ----------------------------------------------------------------
+        consumer = KafkaConsumer(
+            getenv("TOPIC"),
+            bootstrap_servers=[server.strip() for server in getenv("BOOTSTRAP_SERVERS").split(',')],
+            client_id=getenv("CLIENT_ID"),
+            group_id=getenv("GROUP_ID"),
+            auto_offset_reset=getenv("AUTO_OFFSET_RESET"), # 'earliest' or 'latest'
+            enable_auto_commit=bool(getenv("ENABLE_AUTO_COMMIT")),
+            value_deserializer=lambda x: loads(x.decode('utf-8'))
+        )
 
-        # input_data are data received by the module
-        input_data = None
+        for message in consumer:
+            log.debug(f"Input data: {message.value}")
 
+            # send data to the next module
+            send_error = send_data(message.value)
 
-        # ----------------------------------------------------------------
-
-        # send data to the next module
-        send_error = send_data(input_data)
-
-        if send_error:
-            log.error(send_error)
-        else:
-            log.debug("Data sent sucessfully.")
+            if send_error:
+                log.error(send_error)
+            else:
+                log.debug("Data sent successfully.")
 
     except Exception as e:
         log.error(f"Exception in the module business logic: {e}")
